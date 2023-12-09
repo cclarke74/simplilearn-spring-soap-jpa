50dc50aeadfa1961eb030c3edd531fdf6d885bfe
cclarke74 <clairc2@gmail.com>
2023-12-09 14:51:47 -0800

    first commit

---
+++ src/main/java/com/simplilearn/spring/SimplilearnSpringSoapJpaApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplilearnSpringSoapJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplilearnSpringSoapJpaApplication.class, args);
+	}
+
+}
